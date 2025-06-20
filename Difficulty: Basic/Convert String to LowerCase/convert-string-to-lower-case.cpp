34f44718b0916b2ddb5cb75a7969e862ccf0389b
Sruthikj <sruthikj22@gmail.com>
2025-06-20 16:45:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string toLower(string& s) {
+        
+        int l = s.length();
+        for(int i=0;i<l;i++){
+            if(s[i] >= 'A' && s[i] <= 'Z'){
+                s[i] = s[i] + 32;
+            }
+        }
+        return s;
+    }
+};
