f5407f00893e4ddc7ee86b6f3d5ecbdefef52e41
Sruthikj <sruthikj22@gmail.com>
2025-06-29 17:29:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue Push & Pop/queue-push-pop.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    queue<int> fillQ(const vector<int>& arr) {
+        // code here
+        queue<int> q;
+        for(int i=0;i<arr.size();i++){
+            q.push(arr[i]);
+        }
+        return q;
+    }
+
+    void emptyQ(queue<int>& q) {
+        while(!q.empty()){
+            cout<<q.front()<<" ";
+            q.pop();
+        }
+        cout<<endl;
+    }
+};
