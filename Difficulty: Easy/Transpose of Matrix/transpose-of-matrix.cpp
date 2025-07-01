abaaabc436fddeb0cd29d39f247a887879115c57
Sruthikj <sruthikj22@gmail.com>
2025-07-02 00:49:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -1,12 +1,13 @@
 class Solution {
   public:
     // Function to find transpose of a matrix.
-    void transpose(vector<vector<int>>& mat, int n) {
+    void transpose(int mat[4][4]) {
         // code here
-        for(int i=0;i<mat.size();i++){
-            for(int j=i;j<mat.size();j++){
+        for(int i=0;i<4;i++){
+            for(int j=i+1;j<4;j++){
                 swap(mat[i][j],mat[j][i]);
             }
         }
+        mat[4][4];
     }
 };
