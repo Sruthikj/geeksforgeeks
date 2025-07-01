de5eeac40935db323ca0650e651e90487bae2fb9
Sruthikj <sruthikj22@gmail.com>
2025-07-01 11:50:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Queue using two Stacks/queue-using-two-stacks.cpp
+++ Difficulty: Easy/Queue using two Stacks/queue-using-two-stacks.cpp
@@ -1,30 +1,31 @@
-/* The structure of the class is
-class StackQueue{
-private:
-    // These are STL stacks ( http://goo.gl/LxlRZQ )
+// User function Template for C++
+//Back-end complete function Template for C++
+class StackQueue {
+  private:
     stack<int> s1;
     stack<int> s2;
-public:
-    void push(int);
-    int pop();
-}; */
-void StackQueue::push(int B) {
-    s1.push(B);
-}
 
-int StackQueue::pop() {
-    int val;
-    if(s2.empty()){
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        if(s2.empty()) return -1;
-        val = s2.top();
-        s2.pop();
-    }else{
-        val = s2.top();
-        s2.pop();
+  public:
+    void push(int B) {
+        // code here
+        s1.push(B);
     }
-    return val;
-}
+
+    int pop() {
+        int val;
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            if(s2.empty()) return -1;
+            val = s2.top();
+            s2.pop();
+        }
+        else{
+            val = s2.top();
+            s2.pop();
+        }
+        return val;
+    }
+};
