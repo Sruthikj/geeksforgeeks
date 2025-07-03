c1004b634a8865e9f8b68997a640a92ab9513c3a
Sruthikj <sruthikj22@gmail.com>
2025-07-03 23:58:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Parties and seats/parties-and-seats.cpp
+++ Difficulty: Basic/Parties and seats/parties-and-seats.cpp
@@ -1,45 +1,28 @@
+// User function Template for C++
+
 /* print the name of the parties along with seats
    they got in election and print the maximum seat
    received
 */
-// void Election2019(char party[], int seats[], int n) {
-//     // Your code here
-//     map<char,int> mp;
+void Election2019(vector<char>& party, vector<int>& seats) {
     
-//     for(int i=0;i<n;i++){
-//         mp[party[i]] = seats[i];
-//     }
+    // Your code here
+    map<char,int> mp;
     
-//     for(auto it=mp.begin();it!=mp.end();it++){
-//         cout<<it->first<<" "<<it->second<<endl;
-//     }
-    
-//     int winner = INT_MIN;
-//     for(auto it=mp.begin();it!=mp.end();it++){
-//         if((it->second) > winner){
-//             winner = it->second;
-//         }
-//     }
-//     cout<<winner;
-// }
-
-void Election2019(char party[], int seats[], int n) {
-    map<char, int> mp;
-
-    for (int i = 0; i < n; i++) {
-        mp[party[i]] += seats[i];  // Accumulate if duplicate
+    for(int i=0;i<party.size();i++){
+        mp[party[i]] = seats[i];
     }
-
-    for (auto it = mp.begin(); it != mp.end(); ++it) {
-        cout << it->first << " " << it->second << endl;
+    
+    for(auto it=mp.begin();it!=mp.end();it++){
+        cout<<it->first<<" "<<it->second<<endl;
     }
-
-    int maxSeats = INT_MIN;
-    for (auto it = mp.begin(); it != mp.end(); ++it) {
-        if (it->second > maxSeats) {
-            maxSeats = it->second;
+    
+    int winner = INT_MIN;
+    for(auto it=mp.begin();it!=mp.end();it++){
+        if((it->second) > winner){
+            winner = it->second;
         }
     }
+    cout<<winner<<endl;
 
-    cout << maxSeats << endl;
 }
