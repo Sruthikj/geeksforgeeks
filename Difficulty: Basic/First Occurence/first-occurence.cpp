63efea6b286ef17110cc601e17451324654eacac
Sruthikj <sruthikj22@gmail.com>
2025-07-05 13:00:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First Occurence/first-occurence.cpp
@@ -0,0 +1,8 @@
+// Function to locate the occurrence of the string x in the string s.
+class Solution {
+  public:
+    int firstOccurence(string &txt, string &pat) {
+        // Your code here
+        return txt.find(pat);
+    }
+};
