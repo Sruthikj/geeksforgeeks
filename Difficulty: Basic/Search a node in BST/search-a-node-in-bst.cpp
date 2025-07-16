d242672ffbabb967788d340f25dabfd235e78630
Sruthikj <sruthikj22@gmail.com>
2025-07-16 13:54:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Search a node in BST/search-a-node-in-bst.cpp
@@ -0,0 +1,11 @@
+// Function to search a node in BST.
+bool search(Node* root, int x) {
+    // Your code here
+    if(root == NULL){
+        return false;
+    }
+    if(root->data == x){
+        return true;
+    }
+    return search(root->left,x) || search(root->right,x);
+}
