ba68ad56d78ba1e3d709d0af5bab114023dfb5e3
Sruthikj <sruthikj22@gmail.com>
2026-07-07 16:34:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        if(n%3==0 && n%5==0){
+            System.out.print("FizzBuzz");
+        }
+        else if(n%5==0){
+            System.out.print("Buzz");
+        }
+        else if(n%3==0)
+        {
+            System.out.print("Fizz");
+        }
+        else
+        {
+            System.out.print(n);
+        }
+    }
+}
